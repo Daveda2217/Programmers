bafccbbc92e2239cad288eb85e5f53981d2db258
Dave <rladuddnr486@naver.com>
2020-11-23 08:33:58 +0900

    add annotation

--- 전화번호목록.cpp
+++ 전화번호목록.cpp
@@ -7,13 +7,12 @@ bool solution(vector<string> phone_book)
 {
     bool answer = true;
 
-    sort(phone_book.begin(), phone_book.end());
+    sort(phone_book.begin(), phone_book.end()); // sort
 
-    for (int i = 0; i < phone_book.size() - 1; i++)
+    for (int i = 0; i < phone_book.size() - 1; i++) // 0 ~ phone_book.size() - 1
     {
-
-        size_t idx = phone_book[i + 1].find(phone_book[i]);
-        if (idx != string::npos && idx == 0)
+        size_t idx = phone_book[i + 1].find(phone_book[i]); // phone_book[i]를 찾기
+        if (idx != string::npos && idx == 0)                // 찾았고 찾은 index가 0이라면
         {
             answer = false;
             break;
